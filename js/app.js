2990df1dbcc7a7ca177907d461a39235e231a4a8
khadija <khadijahusseinali38@gmail.com>
2023-09-13 00:46:59 +0300

    added

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      autoplay: true,
+      autoplayTimeout: 2000, // 2 seconds between auto-play transitions
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 2
+        },
+        992: {
+          items: 3
+        },
+        
+        1400: {
+          items: 4
+        },
+      }
+    });
+  });
